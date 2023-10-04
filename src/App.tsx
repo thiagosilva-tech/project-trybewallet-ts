4f7221b5982f398aa310f5fdd6d92cb775ca4c10
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-10-04 11:36:42 -0300

    feat: requisito 02

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
+import Header from './components/Header';
 
 function App() {
   return (
     <main>
       <Routes>
         <Route path="/" element={ <Login /> } />
-        <Route path="/carteira" element={ <Wallet /> } />
+        <Route path="/carteira" element={ <Header /> }>
+          <Route path="/carteira" element={ <Wallet /> } />
+        </Route>
       </Routes>
     </main>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,23 @@
+import { useSelector } from 'react-redux';
+import { State } from '../types';
+import './Header.css';
+
 function Header() {
-  return <div>Header</div>;
+  const loginData = useSelector((state: State) => state.user);
+  // const walletData = useSelector((state: State) => state.wallet);
+  return (
+    <header className="header">
+      <h1 className="h1-header">Trybe Wallet</h1>
+      <div>
+        <p data-testid="email-field">{`Email: ${loginData.email}`}</p>
+        <p data-testid="total-field">
+          { `Despesa Total: R$ ${0}` }
+          {' '}
+          <span data-testid="header-currency-field">BRL</span>
+        </p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
