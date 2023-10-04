9eb19494065e0e93c91748df8dd9886b0c84dd4e
ThiagoSilva042 <thiagohenriquedasilva042@gmail.com>
2023-10-04 11:44:56 -0300

    fix: outlet do componente header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { Outlet } from 'react-router-dom';
 import { State } from '../types';
 import './Header.css';
 
@@ -6,17 +7,20 @@ function Header() {
   const loginData = useSelector((state: State) => state.user);
   // const walletData = useSelector((state: State) => state.wallet);
   return (
-    <header className="header">
-      <h1 className="h1-header">Trybe Wallet</h1>
-      <div>
-        <p data-testid="email-field">{`Email: ${loginData.email}`}</p>
-        <p data-testid="total-field">
-          { `Despesa Total: R$ ${0}` }
-          {' '}
-          <span data-testid="header-currency-field">BRL</span>
-        </p>
-      </div>
-    </header>
+    <>
+      <header className="header">
+        <h1 className="h1-header">Trybe Wallet</h1>
+        <div>
+          <p data-testid="email-field">{`Email: ${loginData.email}`}</p>
+          <p data-testid="total-field">
+            { `Despesa Total: R$ ${0}` }
+            {' '}
+            <span data-testid="header-currency-field">BRL</span>
+          </p>
+        </div>
+      </header>
+      <Outlet />
+    </>
   );
 }
 
